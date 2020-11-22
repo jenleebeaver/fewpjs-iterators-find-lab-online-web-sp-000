cce641749e6182f9ec109ce97af478084e7d05ad
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-22 16:54:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 
 superbowlWin = record => {
-  return record.find()
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
 }
 
 superbowlWin = (record) => {
